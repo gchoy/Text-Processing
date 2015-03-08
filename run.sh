6e1f00a25c12f0fb9c4786542eddb0805978ed11
gchoy <gchoy@users.noreply.github.com>
2015-03-08 01:25:21 -0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-#import filename
-#import glob
-#import sys
-#import string
+
 
 chmod a+x ./src/word_count.py
 chmod a+x ./src/running_median.py
 
-# finally I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
+#Executes word count and running median scripts
 python3 ./src/word_count.py ./wc_input ./wc_output/wc_result.txt
 python3 ./src/running_median.py ./wc_input ./wc_output/med_result.txt
