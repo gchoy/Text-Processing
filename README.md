1bcbb3fa5003cc828efab8889afb716ed70307cf
gchoy <gchoy@users.noreply.github.com>
2015-03-07 21:37:02 -0800

    Uploading of all files.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Text-Processing
-Basic text file processing. Includes word count and  running mean for all files in wc_input folder
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#apt-get install python-pandas
+
+chmod a+x ./src/word_count.py
+chmod a+x ./src/running_median.py
+
+# finally I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
+python3 ./src/word_count.py ./wc_input ./wc_output/wc_result.txt
+python3 ./src/running_median.py ./wc_input ./wc_output/med_result.txt
